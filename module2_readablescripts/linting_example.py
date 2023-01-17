22fe9dbbfb733abb55b1f12bf2db641a5d677cc2
ebatty <erb2180@columbia.edu>
2023-01-17 07:30:27 -0500

    add linting example

---
+++ module2_readablescripts/linting_example.py
@@ -0,0 +1,14 @@
+# from https://dsstream.com/improve-your-python-code-quality/
+
+import math;
+globalValue = 0                 
+def Bad_Quality_Function(argument1):
+    a = [1,2,3]
+    a[10] = 10
+    DivideByZero = math.e/0
+    if DivideByZero==0:
+        return None
+    else:
+        return result
+    pass
+Bad_Quality_Function()

1c9b15ba940917f492af44497aa3eae576e23324
ebatty <erb2180@columbia.edu>
2023-01-17 07:46:39 -0500

    add commenting example

---
+++ module2_readablescripts/commenting_example.py
@@ -0,0 +1,43 @@
+# from https://github.com/JuliaDynamics/GoodScientificCodeWorkshop
+
+# validate protein sequence
+a = val(text)
+
+
+# Perform eqs. (19) and (20) from Datseris et al. 2019
+x = 5*y**2 2
+
+
+# TODO: generalize to higher dims
+for i in range(5):
+    pass
+
+
+toks = split(line)
+# toks[5] contains the raw p-value
+# toks[6] the test number
+adj_pval = calculate_adj_pval(toks[5], toks[6])
+
+
+# match genomic regions in the format <id>:<start>-<end>
+genomic_regex = r"([^:]+):(\d+)-(\d+)$"
+
+
+# this takes ~1 hour on local machine
+result = test(arg)
+
+
+if a > 16: # if age is great than 16, print can drink
+    print('can drive')
+else: # otherwise, print cannot drive
+    print('cannot drive')
+
+
+# example from Jeff Raskin
+
+# A binary search turned out to be slower than the 
+# Boyer-Moore algorithm for the data sets of interest, 
+# thus we have used the more complex, but faster method 
+# even though this problem does not at first seem amenable 
+# to a string search technique.
+res = binary_search(arg)
